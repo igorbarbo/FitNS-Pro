c4e00fc4da096db1862f8894012ccc34bf562cbf
Igor Barbosa <igorbarbosapt@gmail.com>
2026-02-28 15:47:21 +0000

    Update and rename nutricao.py to nutrition.py

--- pages/nutricao.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-def show_nutricao(get_data_path):
-    df = pd.read_csv(get_data_path("foods_500.csv"))
-    
-    st.write("### Alimentos disponíveis")
-    st.dataframe(df.head(20))
