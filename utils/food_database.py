31551f7c90317ac1f1fe4304e2931a16a356974e
Igor Barbosa <igorbarbosapt@gmail.com>
2026-03-01 01:03:22 +0000

    Update food_database.py

--- utils/food_database.py
+++ utils/food_database.py
@@ -18,3 +18,23 @@ FOODS = {
     "maçã": {"name": "Maçã", "cal": 52, "prot": 0.3, "carb": 14, "fat": 0.2},
     "leite": {"name": "Leite desnatado", "cal": 35, "prot": 3.4, "carb": 5, "fat": 0.1},  # por 100ml
 }
+# utils/food_database.py
+
+# Lista de alimentos com seus macros por 100g
+FOODS = [
+    {"name": "Frango grelhado", "cal": 165, "prot": 31, "carb": 0, "fat": 3.6},
+    {"name": "Peixe grelhado", "cal": 150, "prot": 25, "carb": 0, "fat": 5},
+    {"name": "Ovo", "cal": 155, "prot": 13, "carb": 1.1, "fat": 11},
+    {"name": "Arroz integral cozido", "cal": 124, "prot": 2.6, "carb": 25.8, "fat": 1},
+    {"name": "Batata doce cozida", "cal": 86, "prot": 1.6, "carb": 20.1, "fat": 0.1},
+    {"name": "Brócolis cozido", "cal": 35, "prot": 2.4, "carb": 5, "fat": 0.4},
+    {"name": "Azeite de oliva", "cal": 884, "prot": 0, "carb": 0, "fat": 100},
+    {"name": "Castanhas", "cal": 607, "prot": 15, "carb": 18, "fat": 54},
+    {"name": "Iogurte grego", "cal": 120, "prot": 10, "carb": 4, "fat": 7},
+    {"name": "Whey protein", "cal": 400, "prot": 80, "carb": 6, "fat": 5},
+    {"name": "Aveia", "cal": 389, "prot": 16.9, "carb": 66.3, "fat": 6.9},
+    {"name": "Pão integral", "cal": 265, "prot": 10, "carb": 45, "fat": 3.5},
+    {"name": "Banana", "cal": 89, "prot": 1.1, "carb": 22.8, "fat": 0.3},
+    {"name": "Maçã", "cal": 52, "prot": 0.3, "carb": 14, "fat": 0.2},
+    {"name": "Leite desnatado", "cal": 35, "prot": 3.4, "carb": 5, "fat": 0.1},
+]
